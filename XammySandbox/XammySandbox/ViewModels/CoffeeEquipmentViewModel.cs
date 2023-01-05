73fd27e05c9df46a58af46a7e63a9f94f327bd96
Bryce Sickles <brycesickles@gmail.com>
2023-01-05 10:31:28 -0600

    uploading changes prior to computer OS wipe, no notable progress

--- XammySandbox/XammySandbox/ViewModels/CoffeeEquipmentViewModel.cs
+++ XammySandbox/XammySandbox/ViewModels/CoffeeEquipmentViewModel.cs
@@ -3,43 +3,50 @@ using MvvmHelpers.Commands;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using XammySandbox.Models;
 
 namespace XammySandbox.ViewModels
 {
     public class CoffeeEquipmentViewModel : BaseViewModel
     {
-        public ObservableRangeCollection<string>Coffee { get; }
+        public ObservableRangeCollection<Coffee>Coffee { get; }
+        //groupings allow for lists of lists 
+        public ObservableRangeCollection<Grouping<string, Coffee>> CoffeeGroups { get; }
+        public AsyncCommand RefreshCommand { get; }
         public CoffeeEquipmentViewModel() 
         {
-            IncreaseCount = new Command(OnIncrease);
-            CallServerCommand = new AsyncCommand(CallServer);
-            Coffee = new ObservableRangeCollection<string>();
-            Title = "Coffee Equipment"; 
-        }
-        public ICommand CallServerCommand { get; }
-        async Task CallServer()
-        {
-            var items = new List<string> { "Yes Please", "Tonx", "Blue-Bottled" }; 
-            Coffee.AddRange(items);
+            Title = "Coffee Equipment";
+
+            Coffee = new ObservableRangeCollection<Coffee>();
+            CoffeeGroups = new ObservableRangeCollection<Grouping<string, Coffee>>();
+
+            var image = "https://media.istockphoto.com/id/1303583671/photo/cup-glass-of-coffee-with-smoke-and-coffee-beans-on-old-wooden-background.jpg?s=612x612&w=0&k=20&c=fAh3m6Hqxz-qeA45Tj2jGARhRiGFhgm80dLVthnvlD8=";
+
+            Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Sip of Sunshine", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Potent Potable", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Potent Potable", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue Bottle", Name = "Kenya Kiambu Handege", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue Bottle", Name = "Kenya Kiambu Handege", Image = image });
+
+            CoffeeGroups.Add(new Grouping<string, Coffee>("Blue Bottle", Coffee.Where(c => c.Roaster == "Blue Bottle")));
+            CoffeeGroups.Add(new Grouping<string, Coffee>("Yes Plz", Coffee.Where(c => c.Roaster == "Yes Plz")));
+
+            RefreshCommand = new AsyncCommand(Refresh);
         }
 
-        public ICommand IncreaseCount { get; }
+        async Task Refresh()
+        {
+            IsBusy = true;
+            await Task.Delay(2000);
+            isBusy = false; 
+        }
+       
 
-        int count = 0;
-        string countDisplay = "Click Me!";
-        public string CountDisplay
-        {
-            get => countDisplay;
-            set => SetProperty(ref countDisplay, value);
-        }
-        void OnIncrease()
-        {
-            count++;
-            CountDisplay = $"You clicked {count} time(s)";
-        }
+        
 
         //example of using setProperty once more
         bool isBusy; 
